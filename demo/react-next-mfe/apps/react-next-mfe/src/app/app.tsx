7b3b40ea58db6c841f271dbdd424a9e46d06217c
h_r_m <pitechworks@gmail.com>
2023-09-26 10:34:07 -0700

    added react host

---
+++ demo/react-next-mfe/apps/react-next-mfe/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-next-mfe" />
+    </div>
+  );
+}
+
+export default App;
